8b4449833a8c7545bca4958e7bcf120ccf8fcae9
Dasa-Alzate <david.s356811@cesurformacion.com>
2025-02-18 00:55:46 +0100

    banco de 100 preguntas

--- app.js
+++ app.js
@@ -27,14 +27,13 @@ function renderizarPregunta() {
   let preguntaActual = preguntas[indicePregunta];
   let contenido = `<h2 class='text-lg font-bold mb-4'>${preguntaActual.pregunta}</h2>`;
 
-  // Verifica si hay código en la pregunta y lo agrega
   if (preguntaActual.codigo) {
     contenido += `<div class="codigo bg-gray-800 text-emerald-300 p-4 rounded-md w-full overflow-x-auto sm:overflow-x-hidden"><pre class="whitespace-pre-wrap break-words w-full"><code class="w-full">${preguntaActual.codigo.texto}</code></pre></div>`;
   }
 
   if (modo === "multiple") {
     preguntaActual.respuestas.forEach((resp, i) => {
-      contenido += `<button onclick="seleccionarRespuesta(${resp.esCorrecto})" class='w-full p-2 my-1 bg-blue-400 text-white rounded'>${resp.texto}</button>`;
+      contenido += `<button onclick="seleccionarRespuesta(${resp.esCorrecto}, this)" class='w-full p-2 my-1 bg-blue-400 text-white rounded'>${resp.texto}</button>`;
     });
   } else {
     contenido += `<button onclick="mostrarRespuesta()" class='w-full p-2 bg-green-500 text-white rounded'>Ver respuesta</button>`;
@@ -44,8 +43,27 @@ function renderizarPregunta() {
 }
 
 
-function seleccionarRespuesta(correcto) {
+function seleccionarRespuesta(correcto, selectedButton) {
+  const buttons = document.querySelectorAll("#app button");
+  buttons.forEach(button => button.disabled = true);
+
+  const respuestaCorrecta = preguntas[indicePregunta].respuestas.find(r => r.esCorrecto).texto;
+
+  buttons.forEach(button => {
+    if (button.textContent === respuestaCorrecta) {
+      button.classList.add("bg-emerald-500"); 
+    } else if (button === selectedButton && !correcto) {
+      button.classList.add("bg-rose-500"); 
+    }
+  });
+
   if (correcto) puntaje++;
+
+  const avanzarButton = `<button onclick="avanzarSiguientePregunta()" class='w-full p-2 mt-4 bg-blue-500 text-white rounded'>Siguiente pregunta</button>`;
+  document.getElementById("app").innerHTML += avanzarButton;
+}
+
+function avanzarSiguientePregunta() {
   indicePregunta++;
   renderizarPregunta();
 }
